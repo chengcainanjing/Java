4a003cbc69a296eb61860b4bba493afd9f58a181
chengcainanjing <chengcainuist@gmail.com>
2019-11-22 09:23:43 +0800

    枚举类代码演示，添加备注

--- 练习/A20191121EnumTest.java
+++ 练习/A20191121EnumTest.java
@@ -12,6 +12,7 @@ public class A20191121EnumTest {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a size: (SMALL, MEDIUM,LARGE,EXTAR_LARGE)");
         String input = in.next().toUpperCase();
+        // 返回指定名字、给定类的枚举常量
         Size size = Enum.valueOf(Size.class, input);
         System.out.println("size= " + size);
         System.out.println("abbreviation=" + size.getAbbreviation());
@@ -23,6 +24,7 @@ public class A20191121EnumTest {
 
 // 枚举类
 enum Size {
+    //定义了四个实例
     SMALL("S"), MEDIUM("M"), LARGE("L"), EXTRA_LARGE("XL");
 
     private Size(String abbreviation) {
