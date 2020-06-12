d3eb83c088a844690b0c7bf9a88083d0ad32e458
chengcai <0049002929@znv.com>
2020-06-12 15:10:39 +0800

    更新代码

--- 练习/day13/StringMethodDemo.java
+++ 练习/day13/StringMethodDemo.java
@@ -16,6 +16,70 @@ class StringMethodDemo {
 
         method_is();
 
+        method_trans();
+
+        method_replace();
+
+        method_split();
+
+        method_sub();
+
+        method_compare();
+
+    }
+
+    public static void method_compare() {
+        String s1 = "    hello java   ";
+
+        sop(s1.toUpperCase());
+        sop(s1.toLowerCase());
+        sop(s1.trim());
+
+        String s2 = "ada";
+        String s3 = "acd";
+
+        sop(s2.compareTo(s3));
+
+    }
+    public static void method_sub() {
+        String s1 = "abcdfghikjlmn";
+
+        sop(s1.substring(2));
+        sop(s1.substring(2, 5));
+    }
+
+    public static void method_split() {
+        String s1 = "abcd,efgh,ijkl,mn";
+
+        String[] arr1 = s1.split(",");
+
+        for (int i = 0; i < arr1.length; i++) {
+            sop("arr[" + i + "]= " + arr1[i]);
+        }
+    }
+
+    public static void method_replace() {
+        String s1 = "abcdefghijklmn";
+        sop("s1=" + s1);
+
+        String s2 = s1.replace("abcd", "dcba");
+        sop("s2=" + s2);
+    }
+
+    public static void method_trans() {
+        char[] arr = {'a', 'b', 'c', 'd', 'f'};
+        String s1 = new String(arr);
+        sop(s1);
+
+        String s2 = new String(arr, 1, 3);
+        sop(s2);
+
+        String s3 = "acdealskfjklf";
+        char[] a2 = s3.toCharArray();
+
+        for (int i = 0; i < a2.length; i++) {
+            sop("a2[" + i + "]=" + a2[i]);
+        }
     }
 
     public static  void method_is() {
